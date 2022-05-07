723afbf4e574f7348b1b0c2d70e0cd3a6535a160
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-05-07 20:09:50 +0900

    remove threashold param

--- org.langrid.service.ml/src/main/java/org/langrid/service/ml/ImageClassificationService.java
+++ org.langrid.service.ml/src/main/java/org/langrid/service/ml/ImageClassificationService.java
@@ -2,6 +2,5 @@ package org.langrid.service.ml;
 
 public interface ImageClassificationService {
 	ImageClassificationResult[] classify(
-			String format, byte[] image, String labelLanguage,
-			double threashold, int maxResults);
+			String format, byte[] image, String labelLanguage, int maxResults);
 }
