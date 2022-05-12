34ba05ccd8eac7a174d0045b31de6fa41d75a7a5
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-05-12 22:26:37 +0900

    add throws.

--- org.langrid.service.ml/src/main/java/org/langrid/service/ml/ImageClassificationService.java
+++ org.langrid.service.ml/src/main/java/org/langrid/service/ml/ImageClassificationService.java
@@ -1,6 +1,11 @@
 package org.langrid.service.ml;
 
+import jp.go.nict.langrid.service_1_2.InvalidParameterException;
+import jp.go.nict.langrid.service_1_2.ProcessFailedException;
+import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
+
 public interface ImageClassificationService {
 	ImageClassificationResult[] classify(
-			String format, byte[] image, String labelLanguage, int maxResults);
+			String format, byte[] image, String labelLanguage, int maxResults)
+	throws InvalidParameterException, ProcessFailedException, UnsupportedLanguageException;
 }
--- org.langrid.service.ml/src/main/java/org/langrid/service/ml/ObjectDetectionService.java
+++ org.langrid.service.ml/src/main/java/org/langrid/service/ml/ObjectDetectionService.java
@@ -1,6 +1,11 @@
 package org.langrid.service.ml;
 
+import jp.go.nict.langrid.service_1_2.InvalidParameterException;
+import jp.go.nict.langrid.service_1_2.ProcessFailedException;
+import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
+
 public interface ObjectDetectionService {
 	ObjectDetectionResult[] detect(
-			String imageFormat, byte[] image, String labelLanguage, int maxResults);
+			String imageFormat, byte[] image, String labelLanguage, int maxResults)
+	throws InvalidParameterException, ProcessFailedException, UnsupportedLanguageException;
 }
