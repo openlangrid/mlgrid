fa6cee548a1223c336a1d4289a2195976e159670
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2022-05-12 22:29:16 +0900

    remove threshold and add throws.

--- org.langrid.service.ml/src/main/java/org/langrid/service/ml/FacialExpressionRecognitionService.java
+++ org.langrid.service.ml/src/main/java/org/langrid/service/ml/FacialExpressionRecognitionService.java
@@ -1,6 +1,11 @@
 package org.langrid.service.ml;
 
+import jp.go.nict.langrid.service_1_2.InvalidParameterException;
+import jp.go.nict.langrid.service_1_2.ProcessFailedException;
+import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
+
 public interface FacialExpressionRecognitionService {
 	FacialExpressionRecognitionResult[] recognize(
-			String format, byte[] image, String labelLanguage, double threashold, int maxResults);
+			String format, byte[] image, String labelLanguage, int maxResults)
+	throws InvalidParameterException, ProcessFailedException, UnsupportedLanguageException;
 }
