5cad008e8d155064da5b89c1d5c63e6aa3368102
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2023-03-30 01:09:34 +0900

    update interface

--- org.langrid.service.ml/src/main/java/org/langrid/service/ml/ImageClassificationService.java
+++ org.langrid.service.ml/src/main/java/org/langrid/service/ml/ImageClassificationService.java
@@ -6,6 +6,6 @@ import jp.go.nict.langrid.service_1_2.UnsupportedLanguageException;
 
 public interface ImageClassificationService {
 	ImageClassificationResult[] classify(
-			String format, byte[] image, String labelLanguage, int maxResults)
+			byte[] image, String format, String labelLanguage, int maxResults)
 	throws InvalidParameterException, ProcessFailedException, UnsupportedLanguageException;
 }
